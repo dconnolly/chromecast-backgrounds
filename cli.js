51f85f01556539f1a01c8afd6d8fd6a05cbc6b95
Deirdre Connolly <durumcrustulum@gmail.com>
2014-02-24 16:25:53 -0500

    Add command flags, including to download all images

--- cli.js
+++ cli.js
@@ -1,6 +1,105 @@
 #!/usr/bin/env node
 'use strict';
 
-var scrapeChromecastBackgrounds = require('./index');
+var _ = require('lodash');
+var Q = require('q');
+var fs = require('fs');
+var request = require('request');
+var nopt = require('nopt');
+var getChromecastBackgrounds = require('./index');
 
-scrapeChromecastBackgrounds();
+var read = Q.denodeify(fs.readFile);
+var write = Q.denodeify(fs.writeFile);
+
+var saveObjectToFile = function(filename, content) {
+    var jsonString = JSON.stringify(content);
+    write(filename, jsonString, function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        if (data) {
+            console.log(data);
+        }
+    });
+};
+
+var writeInlineMarkdown = function(filename, backgrounds) {
+    var content = '';
+    _(backgrounds).each(function(backgroundEntry) {
+        content += '![]('+backgroundEntry.url+')\n';
+    });
+    write(filename, content);
+};
+
+var updateSize = function(sizeString, backgrounds) {
+    var sizeRegex = /\/s\d+.*?\//;
+    _(backgrounds).each(function(backgroundEntry) {
+        backgroundEntry.url = backgroundEntry.url.replace(sizeRegex, '/'+sizeString+'/');
+    });
+};
+
+var downloadImages = function(backgrounds, directory) {
+    var promises = [];
+    fs.existsSync(directory) || fs.mkdirSync(directory);
+    _.each(backgrounds, function(backgroundEntry) {
+        var deferred = Q.defer();
+        promises.push(deferred.promise);
+        var filename = directory + '/' + decodeURIComponent(backgroundEntry.url.split('/').pop());
+        request(backgroundEntry.url)
+            .pipe(fs.createWriteStream(filename))
+            .on('close', function() {
+                console.log(filename);
+                deferred.resolve();
+            });
+    });
+    Q.all(promises).done(function() {
+        console.log('All done!');
+    });
+};
+
+var options = nopt({
+    size: String,
+    load: String,
+    save: String,
+    writemd: String,
+    download: String,
+    help: Boolean,
+    verbose: Boolean
+}, {
+    h: '--help',
+    v: '--verbose'
+});
+
+if (options.help) {
+    console.log('chromecast-backgrounds --download=<directory> --size=<size> --save=<file> --writemd=<file>');
+    return;
+}
+
+getChromecastBackgrounds().then(function(backgrounds) {
+    if (options.size) {
+        console.log('Updating sizes to ', options.size);
+        backgrounds = updateSize(options.size, backgrounds);
+    }
+    if (options.load) {
+        console.log('Loading previous backgrounds from ', options.load);
+        var backgroundsFromJSON;
+        read(options.load, 'utf8').then(function(backgroundsJSON) {
+            backgroundsFromJSON = JSON.parse(backgroundsJSON);
+            backgrounds = _.uniq(_.union(backgrounds, backgroundsFromJSON), 'url');
+        });
+    }
+    if (options.save) {
+        console.log('Writing backgrounds JSON to ', options.save);
+        saveObjectToFile(options.save, backgrounds);
+    }
+    if (options.writemd) {
+        console.log('Writing backgrounds as inline markdown to ', options.writemd);
+        writeInlineMarkdown(options.writemd, backgrounds);
+    }
+    if (options.download) {
+        downloadImages(backgrounds, options.download);
+    }
+    if (options.verbose) {
+        console.log(backgrounds);
+    }
+});
--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 'use strict';
 
-var fs = require('fs');
-var _ = require('lodash');
 var Q = require('q');
-
-var read = Q.denodeify(fs.readFile);
-var write = Q.denodeify(fs.writeFile);
 var request = Q.denodeify(require('request'));
 
 var chromecastHomeURL = 'https://clients3.google.com/cast/chromecast/home/v/c9541b08';
 var initJSONStateRegex = /(JSON\.parse\(.+'\))/;
-var backgroundsJSONFile = 'backgrounds.json';
 
 var parseChromecastHome = function(htmlString) {
     var JSONParse = htmlString.match(initJSONStateRegex)[1];
@@ -26,42 +20,8 @@ var parseChromecastHome = function(htmlString) {
     return parsedBackgrounds;
 };
 
-var updateSize = function(sizeString, backgrounds) {
-    var sizeRegex = /\/s\d+.*?\//;
-    _(backgrounds).each(function(backgroundEntry) {
-        backgroundEntry.url = backgroundEntry.url.replace(sizeRegex, '/'+sizeString+'/');
-    });
-};
-
-var saveObjectToFile = function(filename, content) {
-    var jsonString = JSON.stringify(content);
-    write(filename, jsonString, function(err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        if (data) {
-            console.log(data);
-        }
-    });
-};
-
-var writeInlineMarkdown = function(filename, backgrounds) {
-    var content = '';
-    _(backgrounds).each(function(backgroundEntry) {
-        content += '![]('+backgroundEntry.url+')\n';
-    });
-    write(filename, content);
-};
-
 module.exports = function() {
-    Q.all([read(backgroundsJSONFile, 'utf8'), request(chromecastHomeURL)])
-        .spread(function(backgroundsJSON, requestResult) {
-            var backgrounds = JSON.parse(backgroundsJSON);
-            var parsed = parseChromecastHome(requestResult[1]);
-            parsed = updateSize('s2560', parsed);
-            backgrounds = _.uniq(_.union(backgrounds, parsed), 'url');
-            console.log('Writing: ', backgrounds);
-            saveObjectToFile(backgroundsJSONFile, backgrounds);
-            writeInlineMarkdown('README.md', backgrounds);
-        });
+    return request(chromecastHomeURL).then(function(requestResult) {
+        return parseChromecastHome(requestResult[1]);
+    });
 };
